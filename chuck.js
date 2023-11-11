a181a80227ae2e82521ac0f537c909ea4221d3a4
Raphael Moynat <raphael.moynat@gmail.com>
2023-11-11 12:43:21 +0100

    chuck

---
+++ chuck.js
@@ -0,0 +1,48 @@
+const boutonChuck = document.querySelector('.boutonChuck')
+const texteBlague = document.querySelector('.chuck')
+
+async function vaChercherUneBlagueSurChuckNorris()
+{
+    return await fetch("https://api.chucknorris.io/jokes/random")
+        .then(response=>response.json())
+        .then(data=>{
+            return data
+        })
+}
+
+boutonChuck.addEventListener("click",()=>{
+    vaChercherUneBlagueSurChuckNorris().then(data=> {
+        let template = `<h3 class="p-2 text-center">${data.value}</h3>`
+        texteBlague.innerHTML += template;
+    })
+})
+
+const divButtons = document.querySelector('.boutonsCategories')
+
+fetch("https://api.chucknorris.io/jokes/categories")
+    .then(response=>response.json())
+    .then(data=> {
+
+        data.forEach((category)=>{
+            console.log(category)
+            let templateButton = `
+        <button class="btn btn-primary boutonCategorie" id="${category}">${category}</button>
+        `
+            divButtons.innerHTML += templateButton
+        })
+
+        let boutons = document.querySelectorAll('.boutonCategorie')
+
+        boutons.forEach((bouton)=>{
+
+            bouton.addEventListener("click", ()=>{
+
+                fetch(`https://api.chucknorris.io/jokes/random?category=${bouton.id}`)
+                    .then(response=>response.json())
+                    .then(data =>{
+                        let template = `<h3 class="p-2 text-center">${data.value}</h3>`
+                        texteBlague.innerHTML += template
+                    })
+            })
+        })
+    })
