10eaa37659ce71b4f9b97bd317777816a01c505d
Raphael Moynat <raphael.moynat@gmail.com>
2023-11-12 20:21:54 +0100

    meteo fonds

--- meteo.js
+++ meteo.js
@@ -5,6 +5,7 @@ const ville = document.querySelector('.nomVille')
 const temp = document.querySelector('.temp')
 const description = document.querySelector('.description')
 const formulaire = document.querySelector('form')
+const fondMeteo = document.querySelector('.fondMeteo')
 
 function apiCall (city){
     let url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`
@@ -15,9 +16,29 @@ function apiCall (city){
         ville.innerHTML = "Ville : " + data.name
         temp.innerHTML = "Température : " + data.main.temp + " °"
         description.innerHTML = "Etat : " + data.weather[0].description
+        console.log(data.weather[0].description)
+        changeFond(data.weather[0].description)
 
     })
 
+}
+
+function changeFond(description){
+    fondMeteo.classList.remove("fondSoleil", "fondNuage", "fondPluie", "fondOrage")
+    if (description.includes("soleil")
+        || description.includes("ciel dégagé")) {
+        fondMeteo.classList.add("fondSoleil")
+    } else if (description.includes("nuageux")
+        || description.includes("couvert")
+        || description.includes("brume")) {
+        fondMeteo.classList.add("fondNuage")
+    } else if (description.includes("pluie")
+        || description.includes("légère pluie")
+        || description.includes("petites averses")) {
+        fondMeteo.classList.add("fondPluie")
+    } else if (description.includes("orage")) {
+        fondMeteo.classList.add("fondOrage")
+    }
 }
 
 formulaire.addEventListener('submit', function (e){
@@ -26,3 +47,5 @@ formulaire.addEventListener('submit', function (e){
     apiCall(ville)
 
 })
+
+apiCall("Lyon")
